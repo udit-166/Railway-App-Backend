e5d5b338fd8e60be7fa82d6dc0d38e68334ec8fb
Udit Shahi <udhishahi1606@gmail.com>
2024-11-02 12:10:42 +0530

    Updated code of registry service

---
+++ src/main/java/com/lcwd/service/registry/ServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.lcwd.service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@EnableEurekaServer
+@CrossOrigin(origins = "*")
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
